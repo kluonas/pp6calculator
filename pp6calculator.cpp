be7c27da2ca86556dc2bc42778951ce329a4d1b5
Adomas <a.jelinskas@warwick.ac.uk>
2014-11-16 17:31:39 +0000

    Add handling input errors

--- pp6calculator.cpp
+++ pp6calculator.cpp
@@ -2,12 +2,15 @@
 #include <cmath>
 #include "PP6Math.hpp"
 #include "PP6Math.hpp"
-
+#include <climits>
+#include <cstdio>
 int main(){
 
-  std::cout<<"This is a pp6calculator\nWhat do you want to do?\n";
-  std::cout<<"\
-              1 - Sum\n"<<std::endl;
+do {
+  std::cout<<std::endl<<"This is a pp6calculator\nWhat do you want to do?\n";
+  std::cout<<"\n\
+              1 - Sum\n\
+              q - quit"<<std::endl;
 /*
               2 - Subtract\n\
               3 - Multiply\n\
@@ -17,16 +20,41 @@ int main(){
               q - quit"<<std::endl;
 */
   char function_choice;
-  std::cin>>function_choice;
+
+  if(!(std::cin>>function_choice)){
+    std::cin.clear();
+    std::cin.ignore(INT_MAX, '\n'); 
+    std::cout<<"[Error] Error 1: input failed"; return 1;
+  }
+  std::cin.clear();
+  std::cin.ignore(INT_MAX, '\n');
+
   switch (function_choice){
-    case '1': {std::cout<<"Enter two numbers you want to sum\n";float a,b,answer=0; std::cin>>a>>b; Sum(a,b,answer); std::cout<<answer<<std::endl; break;}
+    case '1': {std::cout<<"Enter two numbers you want to sum\n"; 
+               float a,b,answer;                               // reserve space for summands and answer 
+               std::cin>>a>>b;                                   // read input
+               if(!std::cin){
+                 std::cout<<"[Error] Error 1: input failed\nReturn to main menu"<<std::endl;
+                 std::cin.clear();
+                 std::cin.ignore(INT_MAX, '\n');
+                 continue;
+                 }
+               else {
+                 Sum(a,b,answer);                                  // call Sum function
+                 std::cout<< a <<" + "<< b <<" = "<< answer << std::endl; // output answer
+                 }
+               break;
+               }
 //    case 2: Subtract();
 //    case 3: Multiply();
 //    case 4: Divide();
 //    case 5: {std::cout<<"Enter number for square root\n"; float number; std::cin>>number; std::cout<<Sqroot(number)<<std::endl;break;}
 //    case 6: {std::cout<<"feed me some ints\n"; int size=0; int a[100]; while(std::cin>>a[size]){size++;}; Sort(size, a);std::cout<<"Sorted array\n";for(int i=0; i<size;++i)std::cout<<a[i]<<std::endl;break;}
-    default: break;
+      case 'q': return 0;
+      default: std::cout<<"Wrong input, try again\n";
   }
+} while (true);
+
 
   return 0;
 }
